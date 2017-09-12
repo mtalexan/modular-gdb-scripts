01ad8268680a6b7343763a3a1d923182a7294d4c
Alexander,Michael <Michael.Alexander@karlstorz.com>
2017-09-12 09:49:36 -0700

    update path to toolchain for uncoring

--- uncore_dm.sh
+++ uncore_dm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PATH=${PATH}:/opt/codesourcery/arm-2009q1-203/bin
+export PATH=${PATH}:/opt/gcc/crosstoolng/5.2.0/arm-none-linux-gnueabi/bin
 
 ELF_TO_DEBUG=${DEVDIR}/myapps/ksi-dmapp/src/bin/arm/dmApp
 PATH_TO_LIBS=${DEVDIR}/fs/fs/
